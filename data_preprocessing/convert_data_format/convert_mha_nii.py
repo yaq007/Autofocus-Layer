617a22549a67d8132fadbc5c8826e9b6afa2cde7
Yao Qin <yaoqin@Yaos-MacBook-Pro.local>
2019-03-08 20:18:35 -0800

    add the example code for converting data formats

--- data_preprocessing/convert_data_format/convert_mha_nii.py
+++ data_preprocessing/convert_data_format/convert_mha_nii.py
@@ -1,7 +1,7 @@
 import SimpleITK as sitk
-data_mha = open('dataform_mha.txt', 'r')
+data_mha = open('data_mha.txt', 'r')
 mha_dir = data_mha.readlines()
-data_nii = open('dataform_nii.txt', 'r')
+data_nii = open('data_nii.txt', 'r')
 nii_dir = data_nii.readlines()
 
 for i in range(len(mha_dir)):
