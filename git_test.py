2935eef20736b6ed833c2b63557cc78780de8ebc
Yao Qin <yaoqin@google.com>
2019-05-13 11:10:18 -0400

    test git

---
+++ git_test.py
@@ -0,0 +1,18 @@
+"""Tests for git."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import git
+from google3.testing.pybase import googletest
+
+
+class GitTest(googletest.TestCase):
+
+  def test_give_me_a_name(self):
+    pass
+
+
+if __name__ == '__main__':
+  googletest.main()
